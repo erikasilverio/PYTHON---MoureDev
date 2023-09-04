53671443cf1c5b7ff73b6167b832a4d5b4b11ac0
Erika Silverio <cms.silverio@gmail.com>
2023-09-04 12:10:08 -0300

    COMMIT

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -7,15 +7,29 @@ if my_condition: # es lo mismo que if my_condition = True:
     print("Se Ejecuta la condicion del IF")
 
 
-my_condition = 5 * 2 
+my_condition = 5 * 5
 
 if my_condition == 10:
     print("Se Ejecuta la condicion del segundo IF")
 
-if my_condition >= 10:
-    print("Se Ejecuta la condicion del tercer IF")
 
-if my_condition > 10:
-    print("Se Ejecuta la condicion del tercer IF")
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
 
-print("La Ejecution Continua")
+elif my_condition == 25:
+    print("Es igual a 25")
+
+
+else:
+    print("Es menor o igual que 10 o mayor o igual a 20 o distinto de 25")
+
+
+print("La ejecucion continua")
+
+my_string = ""
+
+if not my_string:
+    print("Mi cadena de texto es vacia")
+
+if my_string == "Mi cadena de Textoooooo":
+    print("Estas cadena de texto coinciden")
---
+++ 09_loops.py
@@ -0,0 +1,1 @@
+### LOOPS / BUCLES  ###
