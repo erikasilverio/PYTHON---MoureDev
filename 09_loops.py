5bb7e01efa6c7c7ea5aa71f09509f1c6d77eabd2
Erika Silverio <cms.silverio@gmail.com>
2023-09-08 18:05:51 -0300

    commit

--- 09_loops.py
+++ 09_loops.py
@@ -1,1 +1,23 @@
 ### LOOPS / BUCLES  ###
+
+# While
+
+my_condicion = 0
+
+
+while my_condicion < 10:
+    print(my_condicion)
+    my_condicion += 2
+
+else: # es opcional.
+    print("Mi condicion es mayor o igual que 10")
+
+print("La ejecucion continua")
+
+
+while my_condicion < 20:
+    my_condicion +=2 
+    if my_condicion == 15:
+        print("Mi condicion es 15")
+
+    print("Mi condicion es menor que que 20")
